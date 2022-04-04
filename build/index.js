4cc024f2781011d114ce3a8a6cbe87ebee56c92a
Fred Zhen <13911239075@163.com>
2022-04-04 16:03:09 +0800

    1st edition: JS style code; Added @types/node for std node lib

--- build/index.js
+++ build/index.js
@@ -1,7 +1,27 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
 /**
  * @Author: Fred R. Zhen
  * @Date: 2022/4/4 13:10
  * @Email: fred.zhen@gmail.com
  */
-console.log('hello, Typescript');
+const fs_1 = __importDefault(require("fs"));
+const matches = fs_1.default.readFileSync('football.csv', {
+    encoding: 'utf-8'
+}).split('\n')
+    .map((row) => {
+    return row.split(',');
+});
+let manUnitedWins = 0;
+matches.forEach((match) => {
+    if (match[1] === 'Man United' && match[5] === 'H') {
+        manUnitedWins++;
+    }
+    else if (match[2] === 'Man United' && match[5] === 'A') {
+        manUnitedWins++;
+    }
+});
+console.log(`Man United won: ${manUnitedWins} games!!!`);
--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,24 @@
  * @Date: 2022/4/4 13:10
  * @Email: fred.zhen@gmail.com
  */
-console.log('hello, Typescript');
+import fs from 'fs';
+
+
+const matches = fs.readFileSync('football.csv', {
+  encoding: 'utf-8'
+}).split('\n')
+  .map((row: string ): string[] => {
+    return row.split(',');
+  });
+
+let manUnitedWins = 0;
+
+matches.forEach((match: string[]):void =>{
+ if ( match[1] === 'Man United' && match[5] === 'H') {
+   manUnitedWins++;
+ } else if (match[2] === 'Man United' && match[5] === 'A') {
+   manUnitedWins++;
+ }
+})
+
+console.log(`Man United won: ${manUnitedWins} games!!!`);
